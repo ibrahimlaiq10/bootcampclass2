c540f320843cc546d7533f4cf196cf2ec4f04cf4
ibrahim laiq <ibrahim.laiq10@gmail.com>
2020-08-10 02:12:18 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Hi i'm Ibrahim Laiq
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <a href='https://ibrahimlaiq10.github.io/portfolio/'
         >
-          Learn React
+          My Portfolio
         </a>
-      </header>
+      
     </div>
   );
 }
